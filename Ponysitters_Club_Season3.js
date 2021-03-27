d8122ec3d9200a213d8e2ea234ae1c822936409d
XQ0120 <79135038+XQ0120@users.noreply.github.com>
2021-03-27 23:09:10 +0800

    Update Ponysitters_Club_Season3.js

--- Ponysitters_Club_Season3.js
+++ Ponysitters_Club_Season3.js
@@ -89,7 +89,7 @@ async function executeOneByOne() {
    let jxnclen=CookieJDs.length;
    if (process.env.SYNCURL.indexOf('j0xn0c.js')>0)
     {
-    	jxnclen=1;jxnci=0;//判断惊喜农场脚本,自定义开始和结束的位置
+    	jxnclen=2;jxnci=0;//判断惊喜农场脚本,自定义开始和结束的位置
     	
     	
     }
